360fc8497b8cfc087ca2aefa5ffe0cb2f6622e49
AnshumanSelnox <125632289+AnshumanSelnox@users.noreply.github.com>
2024-02-07 17:40:14 +0530

    Add files via upload

---
+++ src/Component/Home/Updatematch/Match.jsx
@@ -0,0 +1,58 @@
+
+
+
+import ScrollContainer from 'react-indiana-drag-scroll';
+import style from "../../../styles/Style.module.scss"
+import Link from 'next/link';
+function Match(updatematch) {
+  
+    return (
+        <div className={`container-fluid ${style.update_match}`}  >
+        {/* <div style={{width:"100%", overflowX: 'scroll'}}> */}
+            <ScrollContainer  className={style.ScrollContainerRelative} >
+              {/* <div className=''> */}
+              {
+                    updatematch?.updatematch?.map((data, index) => {
+                         return (
+
+                            <div className={` ${style.updatematch }`}  key={index}>
+                                
+                                    <div className={` ${style.grid_row}`}>
+                                <Link href={`cricket-prediction/${data.id}/${data.title.replace(/\s+/g, '-')}`} >
+
+                                        <div className="col">
+                                            {data.title}
+                                        </div>
+                                        <div className="col">
+                                            <p>{data.first_team} vs {data.second_team}</p>
+
+                                        </div>
+                                        <div className="col">
+                                            <span>{data.date}</span> |<span>{data.time}</span>
+                                        </div>
+                                        <div className="col">
+                                            <img src={`https://grand11.in/g11/${data?.team_one_img}`} width="50" height="50" alt="G11-Fantasy " />
+                                            <span >VS</span>
+                                            <img src={"https://grand11.in/g11/"+ data?.team_two_img} width="50" height="50" alt="G11-Fantasy " />
+                                        </div>
+                                        <div className="col">
+
+                                        </div>
+
+                                        <div  className={`${style.location} col`} >
+                                            <p className="city_location"><span>match Location-</span>{data.city}</p>
+                                        </div>
+                                </Link>
+                                    </div>
+                            </div>
+                        )
+                    })
+                }
+              {/* </div> */}
+        </ScrollContainer>  
+        {/* </div> */}
+        </div>
+    );
+}
+
+export default Match;
